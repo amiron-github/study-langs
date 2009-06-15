815c2260cac62e459f61d08e7900e9ff005ccbcc
Ilya Lityuga <ilya.lityuga@gmail.com>
2009-06-15 16:37:26 +0400

    ссылки на скрипты и стили, индивидуальные для разных типов страниц вынесены в заголовки, чтобы валидаторы не ругались

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,6 +4,35 @@ class SiteController < ApplicationController
     @file = '/index.html' if @file == '/'
     layout = @file.match(/\/comments\//) ? 'comments.html.erb' : 'application'
     layout = 'comment_audios.html.erb' if @file.match(/\/comments_audio\//)
+
+    # configure javascripts and stylesheets based on page name
+    @javascripts = []
+    @stylesheets = []
+    case @file
+    when /-exercise\.html$/
+      @javascripts << 'exercises'
+      @stylesheets << 'exercises'
+    when /-test\.html$/
+      @javascripts << 'test'
+      @javascripts << 'test-text'
+      @stylesheets << 'test-style'
+    when /unit\d\d\.html$/
+      @javascripts << 'test'
+      @javascripts << 'test-text'
+      @stylesheets << 'test-style'
+    when /reading-practice\d\.html$/
+      @javascripts << 'test'
+      @javascripts << 'test-text'
+      @stylesheets << 'test-style'
+      @stylesheets << 'stylesounds'
+    when /phonetics\d\d\.html$/
+      @javascripts << 'test-text'
+      @stylesheets << 'test-style'
+      @stylesheets << 'stylesounds'
+    when /(memory|balloon-game)\.html$/
+      @javascripts << 'vocabulary'
+    end
+
     render :file => "pages#{@file}.erb", :layout => layout
   rescue StandardError => e
     logger.warn e
