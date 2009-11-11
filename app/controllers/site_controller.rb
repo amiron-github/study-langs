71229b86b65e2e75c9934bb8d29192846712e7d7
dalles <dalles@dalles.office>
2009-11-11 18:55:45 +0300

    fix template

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -33,7 +33,7 @@ class SiteController < ApplicationController
       @javascripts << 'vocabulary'
     end
 
-    if @file=='/user_profile.html'&& !current_user 
+    if @file=='/user_profile.html' && !current_user 
     redirect_back_or_default('/')
     else
     render :file => "pages#{@file}.erb", :layout => layout
@@ -41,6 +41,6 @@ class SiteController < ApplicationController
 
   rescue StandardError => e
     logger.warn e
-#    render :file => "#{RAILS_ROOT}/public/404.html", :status => '404 Not Found'
+    render :file => "#{RAILS_ROOT}/public/404.html", :status => '404 Not Found'
   end
 end
