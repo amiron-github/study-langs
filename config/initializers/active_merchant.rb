ec126e765d97b60ce0ddc66a0653e5f8459db030
dalles <dalles@dalles.office>
2010-02-10 19:02:01 +0300

    add file_read

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,5 +1,5 @@
-ActiveMerchant::Billing::Base.gateway_mode = :production
+#ActiveMerchane::Billing::Base.gateway_mode = :production
 ActiveMerchant::Billing::Base.integration_mode = :production
 ActiveMerchant::Billing::Base.integration(:paypal)
-ActiveMerchant::Billing::PaypalGateway.pem_file =  "#{RAILS_ROOT}/config/paypal/paypal_cert_pem.txt"
+ActiveMerchant::Billing::PaypalGateway.pem_file =  File.read("#{RAILS_ROOT}/config/paypal/paypal_cert_pem.txt")
 
