8859067cce49ee6a9fc545424044bf9d7d2bd7f6
dalles <dalles@dalles.office>
2010-02-10 18:16:40 +0300

    add paypal certs

--- app/models/order_observer.rb
+++ app/models/order_observer.rb
@@ -7,14 +7,14 @@ class OrderObserver < ActiveRecord::Observer
         end
 	end
         
-        def after_save(order)
-        if order.status=2 
-	if order.type_id>0
-        OrderMailer.deliver_purchase_notification(order)
-	else 
-        OrderMailer.deliver_trial_notification(order)
-	end
-        end
-        end
+  def after_save(order)
+    if order.status===2 
+      	if order.type_id>0
+          OrderMailer.deliver_purchase_notification(order)
+        else 
+          OrderMailer.deliver_trial_notification(order)
+      	end
+    end
+  end
 
 end
--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,4 +1,4 @@
-ActiveMerchant::Billing::Base.gateway_mode = :test
-ActiveMerchant::Billing::Base.integration_mode = :test
-ActiveMerchant::Billing::PaypalGateway.pem_file =  '/paypal/paypal_cert.pem'
+#ActiveMerchant::Billing::Base.gateway_mode = :test
+ActiveMerchant::Billing::Base.integration_mode = :paypal
+ActiveMerchant::Billing::PaypalGateway.pem_file =  "#{RAILS_ROOT}/config/paypal/paypal_cert_pem.txt"
 
