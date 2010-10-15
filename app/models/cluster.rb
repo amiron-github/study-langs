4ad2c5913c7b66ee38a489f903fe493287d869d4
unknown <C:\Program Files\The Bat!\Mail>
2010-10-15 19:05:43 +0400

    sounds improve

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -1,6 +1,8 @@
 class Cluster < ActiveRecord::Base
 has_many :cluster_words
 has_many :words, :through=> :cluster_words
+validates_presence_of :tag
+validates_uniqueness_of :tag
 
   def involves(word)
 	if !words.find(:first, :conditions=> ['word_id=?', word.id])
