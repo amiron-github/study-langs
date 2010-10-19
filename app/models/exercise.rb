4cf1dc9a0021c561231c41303e64b150364a0ebe
unknown <sasha@.(none)>
2010-10-20 01:47:07 +0400

    user results

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -4,11 +4,14 @@ has_many :user_tests,
 		 :primary_key => 'test_id'
 belongs_to :category
 
+validates_presence_of :test_id
+validates_uniqueness_of :test_id
+
 
 EVERYDAY_RU = {
 :name => 'Everyday Russian course', 
 :parts => [
-  {:name=> "Lesson 1", :exercises=> ["c0101013.I.c)", "c0101050.V"]}, 
+  {:name=> "Lesson 1", :exercises=> ["c0101013.I.c)", "c0101024.II.d)","c0101032.III.b)","c0101050.V"]}, 
   {:name=> "Lesson 2", :exercises=> ["c0102013.I.c)","c0102052.V.b)","c0102072.VII.b)","c0102074.VII.d)"]},
   {:name=> "Lesson 3", :exercises=> ["c0103013.I.c)","c0103022.II.b)","c0103033.III.c)","c0103073.VII.c)"]}
 ]
